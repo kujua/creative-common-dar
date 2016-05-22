7e910f8d2e6027784e54039fd9ddcf7f5cfad0a7
Wolfgang Loder <kujuasiokubahatisha@gmail.com>
2016-05-22 20:03:29 +0300

    configuration dialyzer

--- Elixir/Libs/dar_imagelib/mix.exs
+++ Elixir/Libs/dar_imagelib/mix.exs
@@ -2,12 +2,20 @@ defmodule DarImagelib.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :dar_imagelib,
-     version: "0.0.1",
-     elixir: "~> 1.1",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :dar_imagelib,
+      version: "0.0.1",
+      elixir: "~> 1.1",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      dialyzer:
+      [
+         plt_apps: ["erts","kernel", "stdlib", "crypto", "public_key", "mnesia"],
+         flags: ["-Wunmatched_returns","-Werror_handling","-Wrace_conditions", "-Wno_opaque"],
+         paths: ["."]
+      ]
+    ]
   end
 
   # Configuration for the OTP application
@@ -31,13 +39,14 @@ defmodule DarImagelib.Mixfile do
     [
         {:mogrify, path: "~/Projects/mogrify"},
         {:dar_model, path: "~/Projects/creative-common-dar/Erlang/Libs/dar_model"},
-        {:ex_doc, "~> 0.11", only: :dev}
+        {:ex_doc, "~> 0.11", only: :dev},
+        {:dialyxir, "~> 0.3", only: [:dev]}
     ]
   end
 
   def getconstant(c) do
     globdefs = %{
-      respath: "/Users/Wolfgang/Projects/creative-common-dar/Elixir/Libs/dar_imagelib/test/res/"
+      respath: "~/Projects/creative-common-dar/Elixir/Libs/dar_imagelib/test/res/"
     }
     case c do
       :respath -> globdefs.respath
